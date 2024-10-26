8df0c61810142cc13faa175ea1606f3be9b35657
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-10-26 23:51:31 +0300

    Lose function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
 const Boundaries = document.querySelectorAll('.boundary')
 
+
+function lose(){
+    alert("You Lost");
+}
+
+
 for (let i=0;i<Boundaries.length;i++){
     Boundaries[i].addEventListener("mouseover", event => {
     event.target.style.backgroundColor = "#eeeeee";
-    event.target.textContent = "NOOOOOOOOO"
+    lose();
     });
 }
+
 
