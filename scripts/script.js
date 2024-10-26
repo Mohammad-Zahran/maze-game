136a246f4c2d68144bd0358b35c036f07bb09f7b
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-10-26 23:56:30 +0200

    Finihed Last Touches

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,38 @@ const Status = document.getElementById('status');
 const Start = document.getElementById('start');
 const End = document.getElementById('end');
 
+function ResetGame(){
+
+    for (let i = 0; i < Boundaries.length; i++) {
+        Boundaries[i].style.backgroundColor = ""
+    }
+    Status.textContent = "Begin by moving your mouse over the 'S'.";
+
+    document.body.style.backgroundColor = ""
+}
+
 function lose() {
     for (let i = 0; i < Boundaries.length; i++) {
-        Boundaries[i].style.backgroundColor = "#red"
+        Boundaries[i].style.backgroundColor = "red"
     }
+    document.body.style.backgroundColor = "orange"; 
     Status.textContent = "You Lost 😒😒";
-    alert("You Lost");
+    setTimeout(ResetGame, 3000);
 }
 
 function win() {
+    for (let i = 0; i < Boundaries.length; i++) {
+        Boundaries[i].style.backgroundColor = "#8888ff"
+    }
+    document.body.style.backgroundColor = "green";
     Status.textContent = "You Win 😍😍";
-    alert("You Win");
+    setTimeout(ResetGame, 3000);
 }
 
 for (let i = 0; i < Boundaries.length; i++) {
     Boundaries[i].addEventListener("mouseover", lose);
 }
+
 
 End.addEventListener("mouseover", win);
 
