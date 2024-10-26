2dae0f3730a01584916af637b87f11311dc4eae2
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-10-26 23:20:51 +0200

    Updating lose and integrating win

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,25 @@
 const Boundaries = document.querySelectorAll('.boundary')
+const Status = document.getElementById('status')
+const Start = document.getElementById('start')
+const End = document.getElementById('end')
 
 
 function lose(){
+    Status.textContent = "You Lost😒😒"
     alert("You Lost");
+}
+
+function win(){
+    Start.addEventListener("mouseover", event =>{
+        Status.textContent = "You Win😍😍"
+         alert("You Win");
+    })
 }
 
 
 for (let i=0;i<Boundaries.length;i++){
     Boundaries[i].addEventListener("mouseover", event => {
-    event.target.style.backgroundColor = "#eeeeee";
-    lose();
+    lose()
     });
 }
 
